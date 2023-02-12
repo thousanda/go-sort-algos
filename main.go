1753370662c482d36d25b650c175796ecd827012
thousanda <k0k1senda@gmail.com>
2023-02-12 17:53:20 +0900

    return if sorted

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	inSls := []int64{4, 1, 6, 5, 2, 9, 7, 10, 8, 3}
+	// inSls := []int64{4, 1, 6, 5, 2, 9, 7, 10, 8, 3}
+	inSls := []int64{1, 2, 3, 4, 5, 6, 9, 7, 10, 8}
 	fmt.Printf("input: %v\n", inSls)
 	outSls := bubbleSort(inSls)
 	fmt.Printf("output: %v\n", outSls)
@@ -20,15 +21,21 @@ func bubbleSort(inSls []int64) []int64 {
 
 	// ソート開始
 	for i := n - 1; i > 0; i-- { // n-1, n-2, ..., 1 (n-1 loops)
+		fmt.Printf("Loop: %d\n", i)
+		isNoChaned := true
 		for j := 0; j < i; j++ { // 0, 1, 2, ..., i-1 (i loops)
-			fmt.Printf("Loop: %d-%d\n", i, j)
 			if outSls[j] > outSls[j+1] {
 				// j番目とj+1番目を交換
 				tmp := outSls[j]
 				outSls[j] = outSls[j+1]
 				outSls[j+1] = tmp
+				isNoChaned = false
 			}
-			fmt.Printf("%v\n", outSls)
+		}
+		fmt.Printf("%v\n", outSls)
+		// 一度も交換してなかったらソート済みなので返す
+		if isNoChaned {
+			return outSls
 		}
 	}
 
