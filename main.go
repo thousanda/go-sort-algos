f53758bfdea48c697dda9300a9b58456b038e70f
thousanda <k0k1senda@gmail.com>
2023-02-19 13:20:10 +0900

    implement selection sort

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	// inSls := []int64{4, 1, 6, 5, 2, 9, 7, 10, 8, 3}
-	inSlc := []int64{1, 2, 3, 4, 5, 6, 9, 7, 10, 8}
+	inSlc := []int64{4, 1, 6, 5, 2, 9, 7, 10, 8, 3}
+	// inSlc := []int64{1, 2, 3, 4, 5, 6, 9, 7, 10, 8}
 	fmt.Printf("input: %v\n", inSlc)
-	outSlc := bubbleSort(inSlc)
+	// outSlc := bubbleSort(inSlc)
+	outSlc := selectionSort(inSlc)
 	fmt.Printf("output: %v\n", outSlc)
 }
 
@@ -37,6 +38,35 @@ func bubbleSort(inSlc []int64) []int64 {
 		if isNoChaned {
 			return outSlc
 		}
+	}
+
+	return outSlc
+}
+
+func selectionSort(inSlc []int64) []int64 {
+	n := len(inSlc)
+	outSlc := make([]int64, n)
+
+	// コピー
+	for i := 0; i < n; i++ {
+		outSlc[i] = inSlc[i]
+	}
+
+	for i := 0; i < n-1; i++ {
+		// 最小値を探す
+		minIdx := i
+		minVal := outSlc[i]
+		for j := i + 1; j < n; j++ {
+			if outSlc[j] < minVal {
+				minIdx = j
+				minVal = outSlc[j]
+			}
+		}
+		// 最小値を未整列部分の先頭に移動する
+		tmp := outSlc[i]
+		outSlc[i] = outSlc[minIdx]
+		outSlc[minIdx] = tmp
+		fmt.Printf("%v\n---\n", outSlc)
 	}
 
 	return outSlc
